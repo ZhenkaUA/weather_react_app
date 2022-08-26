6d8a220d0d92f74ef83e3e9958ca2ea2c0a03cca
Yevheniia Kramar <104134459+ZhenkaUA@users.noreply.github.com>
2022-08-26 14:07:27 +0300

    added_app

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a
+          href="https://github.com/ZhenkaUA/weather-react_app-master.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        <span> by Yevheniia Kramar</span>
+      </small>
+    </div>
+  );
+}
